260cd2cf3d90f4c06a5d8fb8834e7cc6b7903876
saradchhetri07 <saradchhetri20690@gmail.com>
2024-09-12 15:45:38 +0545

    removed commented user schema

--- server/src/schema/user.schema.ts
+++ server/src/schema/user.schema.ts
@@ -59,19 +59,6 @@ export const createUserBodySchema = Joi.object({
       }
       return value;
     }),
-
-  // role: Joi.string().valid("user", "superUser").required().messages({
-  //   "any.required": "role is required",
-  //   "any.only": "role must be either 'user' or 'superUser'",
-  // }),
-
-  // permissions: Joi.array()
-  //   .items(Joi.string().valid("get", "read", "update", "delete"))
-  //   .required()
-  //   .messages({
-  //     "any.required": "permissions are required",
-  //     "array.includes": "permissions must be one of get,read,update,delete",
-  //   }),
 }).options({
   stripUnknown: true,
 });
@@ -85,35 +72,3 @@ export const loginUserBodySchema = Joi.object({
     "any.required": "password is required",
   }),
 });
-
-// export const updateUserBodySchema = Joi.object({
-//   name: Joi.string().optional(),
-//   email: Joi.string().email().optional().messages({
-//     "string.email": "email should be in valid form",
-//   }),
-
-//   password: Joi.string()
-//     .optional()
-//     .min(8)
-//     .messages({
-//       "string.min": "password should be minimum 8 characters",
-//       "password.uppercase":
-//         "at least one password character should be uppercase",
-//       "password.lowercase":
-//         "at least one password character should be lowercase",
-//       "password.specialcharacters":
-//         "at least one password letter should be uppercaseCharacter",
-//     })
-//     .custom((value, helpers) => {
-//       if (!/[A-Z]/.test(value)) {
-//         return helpers.error("password.uppercase");
-//       }
-//       if (!/[a-z]/.test(value)) {
-//         return helpers.error("password.lowercase");
-//       }
-//       if (!/[!@#$%]/.test(value)) {
-//         return helpers.error("password.specialcharacters");
-//       }
-//       return value;
-//     }),
-// });
